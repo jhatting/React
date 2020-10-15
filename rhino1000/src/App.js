3d86484bdf9ebe02666568350f4dc6a8e026894f
Jarrod Hatting <jarrodhatting@Jarrods-MacBook-Pro-2.local>
2020-10-15 12:02:48 -0700

    rhino1000 landing page

---
+++ rhino1000/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { ReactComponent as Logo } from './logo.svg';
+function App() {
+  return (
+    <div>
+      {/* Logo is an actual React component */}
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
